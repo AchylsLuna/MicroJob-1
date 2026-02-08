e92b191de0c1d3b70c12d5f9ae132dd183753a20
Voltriv <vinluanelijah1@gmail.com>
2026-02-08 19:58:15 +0800

    Remove tracked node_modules and unused files

--- client/src/pages/employer/index.ts
+++
@@ -1,4 +0,0 @@
-// Employer-specific pages
-export { default as PostJob } from './PostJob';
-export { default as Applications } from './Applications';
-export { default as JobPosts } from './JobPosts';
--- client/src/pages/worker/index.ts
+++
@@ -1,3 +0,0 @@
-// Worker-specific pages
-export { default as AppliedJobs } from './AppliedJobs';
-export { default as SavedJobs } from './SavedJobs';
